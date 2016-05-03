affe2c969c1cab25650269b7a957a37d1ce0c5e4
swapnaraja <rswapna81@gmail.com>
2016-05-02 19:07:27 -0700

    Update Driver.java

--- mapReduce/CustomCounters/org/swapna/counters/Driver.java
+++ mapReduce/CustomCounters/org/swapna/counters/Driver.java
@@ -1,3 +1,5 @@
+//Counters are useful for debugging the hadoop code. there are many builtin counters 
+//if we want can add user defined counters
 package org.swapna.counters;
 
 import java.io.IOException;
